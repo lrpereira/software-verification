eb872fc3355e2f58a4ca0619debd820fa6f66926
lrpereira <lrp_@outlook.pt>
2020-06-08 22:20:40 +0100

    tpc sat/smt

--- sat/tseitin.c
+++ sat/tseitin.c
@@ -12,13 +12,16 @@ int main(int argc, char *argv[])
 
   /* arg2->x  arg3->y  arg4->z */
   switch(n) {
+  /* or logic */
   case 1:
-    printf("(-%s %s) && (-%s %s) && (-%s %s %s) 0\n",
+    printf("-%s %s 0\n-%s %s 0\n-%s %s %s 0\n",
            argv[3], argv[2], argv[4], argv[2], argv[2], argv[3], argv[4]);
     break;
+  /* and logic */
   case 2:
-    printf("(-%s %s) && (-%s %s) && (-%s -%s %s) 0\n",
+    printf("-%s %s 0\n-%s %s 0\n-%s -%s %s 0\n",
            argv[2], argv[3], argv[2], argv[4], argv[3], argv[4], argv[2]);
+    break;
   }
 
   return 0;
